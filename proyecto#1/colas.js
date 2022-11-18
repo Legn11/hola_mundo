918aa5993e7cc8e8e7ff7ba68dfacda9bf06b7d8
luis gomez <luis.gomez13@utp.ac.pa>
2022-11-18 15:09:22 -0500

    Boton de mostrar y restablecer completos. Consideraciones y alertas

--- proyecto#1/colas.js
+++ proyecto#1/colas.js
@@ -2,14 +2,21 @@
 
 // variables globales para cola
 let contenedor = [];
-let max = 0;
+let max = 6;
 let frente = 0;
 let final = 0;
 
 function cola() {
   var valor = document.getElementById("numcola").value;
+
+  if (valor > 99 || valor < 0) {
+    alert("El numero introducido debe ser de 1 a 99");
+   }else{
+    if (valor === "") {
+      alert("Debe introducir un numero de 1 a 99 para poder continuar");
+    }else{
   if (frente === 1 && final === max || frente === final + 1 ){
-    console.log("pila llena");
+    alert("pila llena");
   }else{
       if(frente === 0 && final === 0){
         frente = 1;
@@ -25,6 +32,10 @@ function cola() {
           console.log(contenedor);
         }
       }
+    }
+    document.getElementById("numcola").value = "";
+    document.getElementById("resultado").innerHTML = " ";
+}
       // Esta funcion controla la eliminación de elementos.
       function eliminar(){
         var valor = document.getElementById("numcola").value;
@@ -49,17 +60,20 @@ function cola() {
     }
     // Esta funcion muestra el estado actual de la pila y la variable cima.
 function visualizar() {
-  document.getElementById("resultado").innerHTML += "<p>" + "Pila:  " + contenedor + "</p>";
-  document.getElementById("resultado").innerHTML += "<p>" + "cima:  " + cima + "</p>";
+  document.getElementById("resultado").innerHTML += "<p>" + "Cola:  " + contenedor + "</p>";
+  document.getElementById("resultado").innerHTML += "<p>" + "Frente:  " + frente + "  " + "Final:  " + final + "</p>";
+//   document.getElementById("resultado").innerHTML += "<p>" + "final:  " + final + "</p>";
 }
 // Esta funcion vacia el contenido del arreglo y reinicia a 0 la variable cima.
 function restablecer() {
   var item = document.getElementById("numcola").value;
   document.getElementById("resultado").innerHTML = " ";
-  for (let i = 1; cima > 0; i++) {
-    if (cima > 0) {
-      almacen.pop(item);
-      cima = cima - 1;
+  for (let i = 1; final > 0; i++) {
+    if (final > 0 ) {
+      contenedor.pop(contenedor);
+      final  = final - 1;
+      frente = 0;
     }
   }
+
 }
